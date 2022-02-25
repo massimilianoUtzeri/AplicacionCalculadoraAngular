bc6378be7a7920b84838690683ddc3e5ccfed35e
USERSAD\mautzeri <utzerimassimiliano@gmail.com>
2022-02-25 18:29:47 +0100

    Backup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'appCalc';
+  title = 'Mini Calculadora';
+
+  //Declaramos e inicializamos 3 variables (Para hacer un binding bidireccional)
+  
+  numero1:number = 0;
+
+  numero2:number = 0;
+
+  resultado:number = 0;
+
+  sumar():number{
+
+    this.resultado = this.numero1+this.numero2;
+
+    return this.resultado;
+    
+  }
+
+  restar():number{
+
+    this.resultado = this.numero1-this.numero2;
+
+    return this.resultado;
+  }
+
+  multiplicar():number{
+
+    this.resultado = this.numero1*this.numero2;
+
+    return this.resultado;
+  }
+
+  dividir():number{
+
+    this.resultado = this.numero1/this.numero2;
+
+    return this.resultado;
+  }
+
 }
